e16af587111273288c77fe396e61c56266948010
joethehumble <420gamingrt@gmail.com>
2025-01-26 15:51:33 -0600

    Trigger fresh deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';  // Ensure App is imported
+// Removed the unused reportWebVitals import
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />  {/* Ensure App component is rendered */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you ever want to measure performance, uncomment this:
+// reportWebVitals(console.log);
