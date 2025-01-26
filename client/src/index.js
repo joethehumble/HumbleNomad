98dc0bf62c885e4b4d7db79e1e981c432c9d23e5
joethehumble <420gamingrt@gmail.com>
2025-01-26 15:50:49 -0600

    Trigger fresh deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Humble Nomad!</h1>
+        <p>Your travel app for exploring remote campsites.</p>
+      </header>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;  // Ensure this is exported to be used in index.js
