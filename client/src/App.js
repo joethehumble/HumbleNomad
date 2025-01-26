c0d50b0c28eabe988d90fd1e0f0e1ed093ee407d
joethehumble <420gamingrt@gmail.com>
2025-01-26 16:27:03 -0600

    Trigger fresh deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src='/public/logo3.png' alt="Logo" />
-        <h2>
-          Humble Nomad
-        </h2>
-        <p>A BOONDOCKING APP </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
