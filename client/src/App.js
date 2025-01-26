14e8fca78a20b1afa36ee9459268f896655588c0
joethehumble <420gamingrt@gmail.com>
2025-01-26 15:35:45 -0600

    Trigger fresh deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to the Boondocking App!</h1>
-        <p>Your go-to app for finding remote camping spots!</p>
+        <h1>Welcome to Humble Nomad!</h1>
+        <p>Your travel app for exploring remote campsites.</p>
       </header>
     </div>
   );
 }
-
-export default App;
