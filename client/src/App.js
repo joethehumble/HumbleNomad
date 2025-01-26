2fb39ea5bcb09b5bf8798ff5ba2cba7099b02eb8
joethehumble <420gamingrt@gmail.com>
2025-01-25 23:34:13 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Boondocking App!</h1>
+        <p>Your go-to app for finding remote camping spots!</p>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
