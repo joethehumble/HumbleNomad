a671e8016bffcc9a371a2bd228e748c26db5ef74
joethehumble <420gamingrt@gmail.com>
2025-01-26 15:54:00 -0600

    Trigger fresh deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Humble Nomad!</h1>
-        <p>Your travel app for exploring remote campsites.</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
-export default App;  // Ensure this is exported to be used in index.js
+export default App;
